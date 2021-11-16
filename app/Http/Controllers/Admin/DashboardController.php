fb5121b4edb4cbdc822d8c08b2c02c068b002df4
wahiddwi <wahiddwi7@gmail.com>
2021-11-16 20:24:55 +0700

    dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Instansi;
+use App\Modul;
+use App\User;
+use App\Wilayah;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        $total_anggota = User::all()->count();
+        $total_provinsi = Wilayah::all()->count();
+        $total_instansi = Instansi::all()->count();
+        $total_modul = Modul::all()->count();
+        return view('pages.admin.dashboard', compact('total_anggota', 'total_provinsi', 'total_instansi', 'total_modul'));
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -21,7 +21,7 @@
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Anggota</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">40.000 Anggota</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$total_anggota}} Anggota</div>
                   </div>
                   <div class="col-auto">
                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -38,7 +38,7 @@
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Provinsi</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">33 Provinsi</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$total_provinsi}} Provinsi</div>
                   </div>
                   <div class="col-auto">
                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -54,10 +54,10 @@
               <div class="card-body">
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Instansi</div>
                     <div class="row no-gutters align-items-center">
                       <div class="col-auto">
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$total_instansi}} Instansi</div>
                       </div>
                     </div>
                   </div>
@@ -75,8 +75,8 @@
               <div class="card-body">
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Modul</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total_modul }} Modul</div>
                   </div>
                   <div class="col-auto">
                     <i class="fas fa-comments fa-2x text-gray-300"></i>
